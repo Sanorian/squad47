5ce24a0667a8c0b9deb1c550774199d4d255dc99
Sanorian <krajnov.sascha@yandex.ru>
2023-11-24 21:39:45 +0400

    Добавил mysql модуль

--- server.js
+++ server.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const mysql = require("mysql");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+app.get("/getrequest", (req, res)=>{
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "yourusername",
+        password: "yourpassword"
+    });
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        sql = "SELECT * FROM requests WHERE Moderated='no'"
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Result: " + result);
+        });
+      });
 });
 
+app.post("/addrequest", (req, res)=>{
+    name = req.body.name;
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "yourusername",
+        password: "yourpassword"
+    });
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        sql = "INSERT INTO requests () VALUES()"
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Result: " + result);
+        });
+      });
+});
+
+const port = 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(`http://localhost:${port}`);
 });
