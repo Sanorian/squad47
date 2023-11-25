7c3739ad7efe84b90603d3e952cefeca56ae6055
Sanorian <krajnov.sascha@yandex.ru>
2023-11-25 15:10:35 +0400

    Дописал систему рейтинга

--- main.js
+++ main.js
@@ -131,6 +131,107 @@ function goTo(id){
                     <td>подтверждение доп. дохода:</td>
                     <td>${application.IncomeLink1}</td>
                 </tr>`;}
+                let scores = 0, age = new Date().getFullYear() - application.BirthDate.slice(4, 8);
+                // Возраст
+                if (age<=25) {
+                    scores+=5;
+                } else if (26<age<=55){
+                    scores+=1;
+                } else if (56<age<74){
+                    scrores+=3;
+                } else {
+                    scores+=10;
+                }
+                //Семейное положение
+                if (application.IsMarried=="Холост"){
+                    scores+=3;
+                } else if (application.IsMarried=="Женат" || application.IsMarried=="Замужем"){
+                    scores+=2;
+                } else {
+                    scores+=5;
+                }
+                //Наличие детей
+                if (application.HasChildren=="Да"){
+                    scores+=3;
+                } else {
+                    scores+=5;
+                }
+                //Кредитная история клиента
+                let creditRating = getCreditRating();
+                if (creditRating=="Хорошая"){
+                    scores+=1;
+                }else if (creditRating=="Средняя"){
+                    scores+=8;
+                }else if (creditRating=="Плохая"){
+                    scores+=15;
+                }else{
+                    scores+=3;
+                }
+                //Основной источник получения доходов
+                if (application.WorkPlace!="Нет"){
+                    scores+=2;
+                } else{
+                    switch (application.FromAdditionalIncome) {
+                        case "Пенсия":
+                            scores+=2;
+                            break;
+                        case isBuisness()=="Предпринимательство":
+                            scores+=7;
+                            break;
+                        default:
+                            scores+=15;
+                            break;
+                    }
+                }
+                //ПДН
+                let PDN;
+                if (application.AdditionalIncomeDocument){
+                    PDN = getMonthPay()/(application.AdditionalIncome+application.Salary);
+                } else {
+                    PDN = getMonthPay()/(application.Salary);
+                }
+
+                if (PDN<=0.7){
+                    scores+=1;
+                } else if (70<PDN<=95){
+                    scores+=10
+                } else {
+                    scores+=20;
+                }
+                //Общий доход
+                let allIncome = application.Salary + application.AdditionalIncome;
+                if (allIncome>250000){
+                    scores+=3;
+                } else if (101000<allIncome<=250000){
+                    scores+=5;
+                }else if (50000<allIncome<=100000){
+                    scores+=7;
+                } else {
+                    scores+=15;
+                }
+                //Есть ли сбережения
+                if (application.HasMoney=="Да"){
+                    scores+=1;
+                } else {
+                    scores+=5;
+                }
+                let riskLevel;
+                if (scores<=30){
+                    riskLevel= "Низкий";
+                } else if (30<scores<=70){
+                    riskLevel = "Средний";
+                } else {
+                    riskLevel = "Высокий";
+                }
+                document.getElementById("applicationPlace").innerHTML += ` 
+                <tr>
+                    <td>Итого</td>
+                    <td>${scores}</td>
+                </tr>
+                <tr>
+                    <td>Уровень риска:</td>
+                    <td>${riskLevel}</td>
+                </tr>`;
                 document.getElementById("applicationPlace").innerHTML += `</tbody></table>`
                 sessionStorage.setItem("lastID", id);
                 document.getElementById(`application${id}`).classList.add("chosen");
@@ -144,4 +245,16 @@ function goTo(id){
                 }
             }
     });
+}
+
+function getCreditRating(){
+
+}
+
+function getMonthPay(){
+
+}
+
+function isBuisness(){
+
 }
