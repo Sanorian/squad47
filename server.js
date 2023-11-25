3c3e39b51e921279f7c525801ef939db00c151e8
Sanorian <krajnov.sascha@yandex.ru>
2023-11-25 10:22:05 +0400

    Добавил callback-и и метод входа

--- server.js
+++ server.js
@@ -9,21 +9,20 @@ app.post("/getallapplications", (req, res)=>{
 
     var con = getConnection(mysql);
 
-    if (haveAccess(username, password, con)){
-        con.connect(function(err) {
-            if (err) throw err;
+    haveAccess(username, password, con, function(access){
+        if (access){
             let sql = "SELECT * FROM applications WHERE Moderated='no'";
             con.query(sql, function (err, result) {
                 if (err) {
                     res.send({res:"bad", reason: "db"});
                     throw err;
-                }
+                }                
                 res.send({res:"good", data: result});
             });
-        });
-    } else {
-        res.send({res:"bad", reason: "access"});
-    }
+        } else {
+            res.send({res:"bad", reason: "access"});
+        }
+    });
 });
 
 app.post("/getoneapplication", (req, res)=>{
@@ -33,28 +32,27 @@ app.post("/getoneapplication", (req, res)=>{
     
     var con = getConnection(mysql);
 
-    if (haveAccess(username, password, con)){
-        con.connect(function(err) {
-            if (err) throw err;
+    haveAccess(username, password, con, function(access){
+        if (access){
             let sql = `SELECT * FROM applications WHERE ID='${id}'`;
             con.query(sql, function (err, result) {
                 if (err) {
                     res.send({res:"bad", reason: "db"});
                     throw err;
-                }
+                }                
                 res.send({res:"good", data: result});
             });
-        });
-    } else {
-        res.send({res:"bad", reason: "access"});
-    }
+        } else {
+            res.send({res:"bad", reason: "access"});
+        }
+    });
 });
 
 app.put("/addapplication", (req, res)=>{
     const username = req.body.username;
     const password = req.body.password;
 
-    const FIO = req.body.fio;
+    const fio = req.body.fio;
     const registrationAdress = req.body.registrationadress;
     const livingAdress = req.body.livingadress;
     const isMarried = req.body.ismarried;
@@ -71,35 +69,9 @@ app.put("/addapplication", (req, res)=>{
 
     var con = getConnection(mysql);
 
-    if (haveAccess(username, password, con)){
-        con.connect(function(err) {
-            if (err) throw err;
+    haveAccess(username, password, con, function(access){
+        if (access){
             let sql = `INSERT INTO applications (FIO, PassportData, RegistrationAdress, LivingAdress, IsMarried, HasChildren, WorkAdress, WorkTimeInMonths, WorkName, Salary, SalaryDocument, AdditionalIncome, AdditionalIncomeDocument, FromAdditionalIncome, HasMoney) VALUES('${fio}', '${registrationAdress}', '${livingAdress}', '${isMarried}', '${hasChildren}', '${workAdress}', '${workTimeInMonths}', '${workName}', '${salary}', '${salaryDocument}', '${additionalIncome}', '${additionalIncomeDocument}', '${fromAdditionalIncome}', '${hasMoney}')`;
-            con.query(sql, function (err, result) {
-                if (err) {
-                    res.send({res:"bad", reason: "db"});
-                    throw err;
-                }
-                res.send({res:"good"});
-            });
-        });
-    } else {
-        res.send({res:"bad", reason: "access"});
-    }
-});
-
-app.patch("/updateapplication", (req, res)=>{
-    const username = req.body.username;
-    const password = req.query.password;
-    const id = req.query.id;
-    const verdict = req.query.verdict;
-
-    var con = getConnection(mysql);
-
-    if (haveAccess(username, password, con)){
-        con.connect(function(err) {
-            if (err) throw err;
-            let sql = `UPDATE applications SET Moderated='yes' AND Verdict='${verdict}' AND Moderator=(SELECT ID from users WHERE username='${username}' AND password='${password}') WHERE ID='${id}'`;
             con.query(sql, function (err, result) {
                 if (err) {
                     res.send({res:"bad", reason: "db"});
@@ -107,31 +79,79 @@ app.patch("/updateapplication", (req, res)=>{
                 }                
                 res.send({res:"good"});
             });
-        });
-    } else {
-        res.send({res:"bad", reason: "access"});
-    }
+        } else {
+            res.send({res:"bad", reason: "access"});
+        }
+    });
 });
+
+app.patch("/updateapplication", (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    const id = req.body.id;
+    const verdict = req.body.verdict;
+
+    var con = getConnection(mysql);
+
+    haveAccess(username, password, con, function(access){
+        if (access){
+            let sql = `UPDATE applications SET Moderated='yes', Verdict='${verdict}', Moderator=(SELECT ID from users WHERE username='${username}' AND password='${password}') WHERE ID='${id}'`;
+            con.query(sql, function (err, result) {
+                if (err) {
+                    res.send({res:"bad", reason: "db"});
+                    throw err;
+                }                
+                res.send({res:"good"});
+            });
+        } else {
+            res.send({res:"bad", reason: "access"});
+        }
+    });
+});
+
+app.post("/login", (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    var con = getConnection(mysql);
+
+    haveAccess(username, password, con, function(access){
+        if (access){
+            let sql = `SELECT * FROM users WHERE username='${username}' AND password='${password}'`;
+            con.query(sql, function (err, result) {
+                if (err) {
+                    res.send({res:"bad", reason: "db"});
+                    throw err;
+                }                
+                res.send({res:"good", username:username, password: password});
+            });
+        } else {
+            res.send({res:"bad", reason: "access"});
+        }
+    });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
   console.log(`http://localhost:${port}`);
 });
 
-function haveAccess(username, password, con){
+function haveAccess(username, password, con, callback){
     con.connect(function(err) {
         if (err) throw err;
-        sql = `SELECT * FROM users WHERE username='${username}' AND password='${password}`
+        sql = `SELECT * FROM users WHERE username='${username}' AND password='${password}'`;
         con.query(sql, function (err, result) {
             if (err) throw err;
             if (result.length==1){
-                return true;
+                callback(true);
             } else {
-                return false;
+                callback(false);
             }
         });
     });
 }
+
 function getConnection(mysql){
     return mysql.createConnection({
         host: "localhost",
